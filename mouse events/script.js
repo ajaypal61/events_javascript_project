a311e0f329e24f866de15d08ba9207136a62a32a
ajaypal61 <ajaypal2987@gmail.com>
2023-05-13 09:13:23 +0530

    set

--- mouse events/script.js
+++ mouse events/script.js
@@ -1,13 +1,13 @@
 
 
-        const menter = document.getElementById('box');
+const menter = document.getElementById('box');
 
-        menter.addEventListener('mouseenter', () => {
-            menter.style.backgroundColor = 'red';
-            console.log('mouse enter');
-        });
+menter.addEventListener('mouseenter', () => {
+    menter.style.backgroundColor = 'red';
+    console.log('mouse enter');
+});
 
-        menter.addEventListener('mouseleave', () => {
-            console.log('mouse leave');
-            menter.style.backgroundColor = 'pink';
-        })
+menter.addEventListener('mouseleave', () => {
+    console.log('mouse leave');
+    menter.style.backgroundColor = 'pink';
+})
